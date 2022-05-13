3b5a959686a5ce8222d8d98dd8eb426d371046e7
victorhcvp <victorhugovergueiro@gmail.com>
2022-05-13 17:28:17 -0400

    signature 7

--- pages/api/interactions.ts
+++ pages/api/interactions.ts
@@ -67,7 +67,7 @@ export default async function handler(
     return res.status(200).json({ nick, nickLol })
   }
 
-  return res.status(200).json({data});
+  return res.status(200).json(rawBody);
 }
 
 export const config = {
