addc5ee877042a9157e46f16be03844e2bf7d70b
victorhcvp <victorhugovergueiro@gmail.com>
2022-05-12 22:15:10 -0400

    signature verify

--- pages/api/interactions.ts
+++ pages/api/interactions.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import nacl from 'tweetnacl'
+import { buffer } from 'micro'
 
 type DiscordRequest = {
   type: number;
@@ -24,14 +26,37 @@ type Data = {
   nickLol: string;
 }
 
-export default function handler(
-  request: NextApiRequest,
-  response: NextApiResponse<Data>
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-  const data = request.body;
+  const rawBody = (await buffer(req)).toString()
+  const data = JSON.parse(rawBody)
+  const signature = req.headers['x-kc-signature']?.toString()
+  const timestamp = req.headers['X-Signature-Timestamp']?.toString()
+
+  if(!signature || !process.env.PUBLIC_API_KEY) {
+    return res.status(401).end('invalid request signature');
+  }
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + rawBody),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(process.env.PUBLIC_API_KEY, 'hex')
+  )
+
+  if(!isVerified) {
+    return res.status(401).end('invalid request signature');
+  }
 
   const nick = data.member.nick;
   const nickLol = data.data.options[0].value;
 
-  response.status(200).json({ nick, nickLol })
+  return res.status(200).json({ nick, nickLol })
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
